d6602099a6cfc013b13a5c3088a4e7a07fdf67b6
Usuario <Usuario@DESKTOP-7V2OB67>
2022-10-10 22:30:05 +0200

    Primer commit en la rama 2

--- Practica1Calidad/src/Principal.java
+++ Practica1Calidad/src/Principal.java
@@ -1,8 +1,11 @@
+import java.util.Iterator;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		for (int i=0;i<5;i++) {
+			System.out.println("Hola Mundo");
+		}
 
 	}
 
