6b28910f9abbc15cdda23bee6db10b72c3a3dd90
Usuario <Usuario@DESKTOP-7V2OB67>
2022-10-10 22:56:35 +0200

    Primera version en la rama 6

--- Practica1Calidad/src/Principal.java
+++ Practica1Calidad/src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		for (int i=0;i<5;i++) {
-			System.out.println("Hola a todos");
+			System.out.println("Primero en la rama 666");
 		}
 
 	}
