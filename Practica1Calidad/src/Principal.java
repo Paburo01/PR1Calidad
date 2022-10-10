638ea61240074bf37d969ff907b918aed88804c1
Usuario <Usuario@DESKTOP-7V2OB67>
2022-10-10 22:57:23 +0200

    Segunda version en la rama 6

--- Practica1Calidad/src/Principal.java
+++ Practica1Calidad/src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		for (int i=0;i<5;i++) {
-			System.out.println("Primero en la rama 666");
+			System.out.println("Segundo en la rama 6");
 		}
 
 	}
